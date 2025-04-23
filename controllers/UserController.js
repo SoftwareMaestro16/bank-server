779f6db7343b929ea132d54ac0a416966c0665a7
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-24 00:24:23 +0300

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -41,7 +41,7 @@ export const register = async (req, res) => {
     console.log('User saved:', user._id);
 
     const token = jwt.sign({ _id: user._id }, "secret", {
-      expiresIn: '30d',
+      expiresIn: 70,
     });
     console.log('Generated token for user:', user._id);
 
