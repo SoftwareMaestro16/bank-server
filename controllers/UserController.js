36de5bbb32238935b4782b3d40de2b807c3ce59e
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-23 23:25:36 +0300

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,48 +4,57 @@ import UserModel from '../models/User.js';
 import { validationResult } from 'express-validator';
 
 export const register = async (req, res) => {
-    console.log('Register route hit with body:', req.body);
-
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-    
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-    
-        const doc = new UserModel({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            passwordHash: hash,
-            address: req.body.address,
-            phoneNumber: req.body.phoneNumber,
-        })
-    
-        const user = await doc.save();
-
-        const token = jwt.sign({
-            _id: user._id
-        }, 'secret', { expiresIn: 70 });
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token
+      console.log('Register route hit with body:', req.body);
+  
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const existingUser = await UserModel.findOne({
+        $or: [
+          { email: req.body.email },
+          { phoneNumber: req.body.phoneNumber },
+        ],
+      });
+  
+      if (existingUser) {
+        return res.status(400).json({
+          message: 'Пользователь с таким email или номером телефона уже существует',
         });
-
+      }
+  
+      const password = req.body.password;
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+  
+      const doc = new UserModel({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        passwordHash: hash,
+        address: req.body.address,
+        phoneNumber: req.body.phoneNumber,
+      });
+  
+      const user = await doc.save();
+  
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET || 'secret', {
+        expiresIn: '1h',
+      });
+  
+      const { passwordHash, ...userData } = user._doc;
+  
+      res.json({ ...userData, token });
     } catch (err) {
-        console.error('Ошибка при регистрации:', err.message, err.stack); // Подробный лог
-        return res.status(500).json({
-            message: 'Failded to register',
-            error: err.message
-        });
+      console.error('Ошибка при регистрации:', err.message, err.stack);
+      res.status(500).json({
+        message: 'Failed to register',
+        error: err.message,
+      });
     }
-}
+};
 
 export const login = async (req, res) => {
     try {
